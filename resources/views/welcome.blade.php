e5b57bf033a2be4692028594efbe5548ef2928e6
Anil Khatiwada <anilkhatiwada@gmail.com>
2020-03-07 13:12:15 +0545

    EDIT

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    DEMO
+                    ANIL KHATIWADA
                 </div>
 
                 <div class="links">
