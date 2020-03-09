894f9d69646ae26797072c84d90dc104cbf84e24
Anil Khatiwada <anilkhatiwada@gmail.com>
2020-03-09 13:16:34 +0545

    edit

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,34 +3,36 @@
 @section('content')
 <div class="container">
     <div class="row justify-content-center">
-        <div class="col-md-8">
+        <div class="col-md-12">
             <div class="card">
                 <div class="card-header">Dashboard</div>
+            
+                <div class="row">
+                    <div class="col-sm">
+                      One of three columns
+                    </div>
+                    <div class="col-sm">
+                      One of three columns
+                    </div>
+                    <div class="col-sm">
+                      One of three columns
+                    </div>
+                  </div>
+                  <div class="row">
+                    <div class="col-sm">
+                      One of three columns
+                    </div>
+                    <div class="col-sm">
+                      One of three columns
+                    </div>
+                    <div class="col-sm">
+                      One of three columns
+                    </div>
+                  </div>
+
 
                 <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-                    <!-- anil code -->
-                   <table class="table">
-                       <tr>
-                           <th>Name</th>
-                           <th>Address</th>
-                       </tr>
-                       <tr>
-                           <td>Anil Khatiwada</td>
-                           <td>Dillibazar</td>
-                       </tr>
-                       <tr>
-                           <td>Sijan Gimire</td>
-                           <td>Maitidevi</td>
-                       </tr>
-                   </table>
                   
-                    <!-- anil code end -->
-                    You are logged in!
                 </div>
             </div>
         </div>
---
+++ resources/views/homepage.blade.php
@@ -0,0 +1,10 @@
+@extends('layouts.app')
+
+@section('content')
+<div style="background-image: url(images/homepagebig.jpg); margin-top: -30px;" >
+    <h1>Homepage</h1>
+</div>
+<div class="container">
+    
+</div>
+@endsection
